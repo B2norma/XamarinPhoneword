c382e3fd925fda44fca5c130dec944673647913b
Bradley Norman <Bradley.Norman91@gmail.com>
2017-08-23 13:09:44 -0400

    Completed Exercise.

--- XamarinPhoneword/XamarinPhoneword/MainPage.xaml.cs
+++ XamarinPhoneword/XamarinPhoneword/MainPage.xaml.cs
@@ -9,9 +9,61 @@ namespace XamarinPhoneword
 {
     public partial class MainPage : ContentPage
     {
+        Entry phoneNumberText;
+        Button translateButton;
+        Button callButton;
+        string translatedNumber;
+
         public MainPage()
         {
-            InitializeComponent();
+            this.Padding = new Thickness(20, 20, 20, 20);
+
+            StackLayout panel = new StackLayout
+            {
+                Spacing = 15
+            };
+
+            panel.Children.Add(new Label
+            {
+                Text = "Enter a Phoneword:",
+                FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label))
+            });
+
+            panel.Children.Add(phoneNumberText = new Entry
+            {
+                Text = "1-855-XAMARIN",
+            });
+
+            panel.Children.Add(translateButton = new Button
+            {
+                Text = "Translate"
+            });
+
+            panel.Children.Add(callButton = new Button
+            {
+                Text = "Call",
+                IsEnabled = false,
+            });
+
+            translateButton.Clicked += OnTranslate;
+            this.Content = panel;
+        }
+
+        private void OnTranslate(object sender, EventArgs e)
+        {
+            string enteredNumber = phoneNumberText.Text;
+            translatedNumber = XamarinPhoneword.PhonewordTranslator.ToNumber(enteredNumber);
+
+            if(!string.IsNullOrEmpty(translatedNumber))
+            {
+                callButton.IsEnabled = true;
+                callButton.Text = "Call " + translatedNumber;
+            }
+            else
+            {
+                callButton.IsEnabled = false;
+                callButton.Text = "Call";
+            }
         }
     }
 }
